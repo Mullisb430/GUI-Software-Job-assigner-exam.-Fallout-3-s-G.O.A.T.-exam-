d09577df9aaabf7a6326d165ebd5ce70f6e6e551
Brandon Mullis <71196828+Mullisb430@users.noreply.github.com>
2021-04-12 06:35:20 -0400

    Add files via upload

---
+++ GOAT/goatCareers.py
@@ -0,0 +1,29 @@
+Careers = {
+    'Vault Chaplain' : [10, 5, 5, 5, 5, 5, 5, 5, 5, 10, 5, 10, 5, 0, 2, 0, 5, 0, 3, 0, "-They say the G.O.A.T.  never lies.  According to the exam,  you are slated to be the next vault...  chaplain.  God help us all."],
+
+    'Laundry Cannon Operator' : [5, 10, 5, 10, 5, 5, 5, 10, 5, 5, 5, 5, 5, 3, 3, 0, 0, 2, 0, 0, "-Well according to this,  you are in line to be trained as a laundry cannon operator.  First time for everything indeed."],
+
+    'Pedicurist' : [5, 5, 10, 5, 5, 10, 5, 5, 10, 5, 5, 5, 5, 1, 0, 2, 1, 2, 1, 2, "-It is nice to know I can still be surprised.  Pedicurist!  I might have guessed Manicurist,  or even Masseuse.  But apparently you are a foot person."],
+
+    'Waste Management Specialist' : [5, 5, 5, 10, 5, 5, 5, 10, 5, 5, 5, 5, 10, 2, 3, 1, 1, 0, 0, 1, "-It says you are perfectly suited for a career as a Waste Management Specialist.  A specialist,  mind you,  not just a dabbler.  Congratulations!"],
+
+    'Vault Loyalty Inspector' : [5, 5, 5, 5, 10, 5, 5, 5, 5, 5, 10, 10, 5, 0, 2, 1, 5, 0, 0, 0, "-Huh.  Vault Loyalty Inspector...  I thought that had been phased out decades ago.  Well,  sounds like a job right up your alley,  huh?"],
+
+    'Clinical Test Subject' : [5, 5, 5, 5, 5, 10, 5, 5, 5, 5, 5, 10, 10, 1, 1, 0, 1, 0, 0, 5, "-Interesting.  Clinical Test Subject...  Sounds like something you should excel at.  I guess you and your dad will be working together."],
+
+    'Fry Cook' : [10, 5, 5, 10, 5, 5, 10, 5, 5, 5, 5, 5, 5, 1, 2, 2, 0, 0, 2, 1, "-Looks like the diner's going to get a new Fry Cook.  I will just say this once: Hold the mustard,  extra pickles.  Ha ha ha."],
+
+    'Jukebox Technician' : [10, 5, 10, 10, 5, 5, 5, 10, 5, 5, 5, 5, 5, 3, 0, 2, 1, 2, 0, 0, "-Thank goodness.  We are finally getting a new Jukebox Technician.  That thing has not worked right since old Joe Palmer passed."],
+
+    'Pip-boy Programmer' : [5, 5, 5, 5, 5, 5, 5, 5, 10, 10, 5, 5, 10, 0, 2, 1, 0, 5, 0, 0, "-Well,  well.  Pip-boy Programmer,  eh?  Stanely will finally have someone to talk shop with."],
+
+    'Tattoo Artist' : [5, 5, 5, 5, 10, 5, 5, 5, 5, 10, 10, 5, 5, 0, 2, 0, 2, 1, 0, 3, "-Huh.  I wonder who will be brave enough to be your first customer as the vault's new Tattoo Artist?  I promise it will not be me."],
+
+    'Shift Supervisor' : [10, 5, 5, 5, 5, 5, 5, 5, 5, 5, 10, 10, 5, 0, 2, 1, 2, 1, 2, 0, "-Apparently you are management material.  You are going to be trained as a Shift Supervisor.  Could I be talking to the next Overseer?  Stranger things have happened.."],
+
+    'Marriage Counselor' : [5, 5, 5, 5, 10, 5, 5, 5, 5, 5, 10, 10, 5, 0, 1, 1, 5, 1, 0, 0, "-Wow.  Wow.  Says here that your are going to be the vault's Marriage Counselor.  Almost makes me want to get married,  just be able to avail myself of your services."],
+
+    'Little League Coach' : [5, 10, 5, 5, 5, 5, 10, 5, 5, 5, 5, 5, 10, 2, 2, 2, 1, 0, 1, 0, "-I always thought you would have a career in professional sports.  You are the new vault Little League Coach!  Congratulations."],
+
+    'Masseuse' : [10, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 10, 10, 0, 0, 2, 5, 1, 0, 0, "-Looks like you will be putting your..  Physical talents to good use as the vault's new Masseuse"]
+}
